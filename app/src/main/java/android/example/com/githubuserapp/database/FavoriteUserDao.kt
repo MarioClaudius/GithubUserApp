fc5ea0417940bd94db14cc31e050871ca63ea236
Mario Claudius <mcmario7901@gmail.com>
2022-10-04 23:10:54 +0700

    add entity and room database for FavoriteUser

---
+++ app/src/main/java/android/example/com/githubuserapp/database/FavoriteUserDao.kt
@@ -0,0 +1,19 @@
+package android.example.com.githubuserapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface FavoriteUserDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(favoriteUser: FavoriteUser)
+
+    @Update
+    fun update(favoriteUser: FavoriteUser)
+
+    @Delete
+    fun delete(favoriteUser: FavoriteUser)
+
+    @Query("SELECT * FROM favoriteuser ORDER BY id ASC")
+    fun getAllFavoriteUsers() : LiveData<List<FavoriteUser>>
+}
---
+++ app/src/main/java/android/example/com/githubuserapp/repository/FavoriteUserRepository.kt
@@ -0,0 +1,33 @@
+package android.example.com.githubuserapp.repository
+
+import android.app.Application
+import android.example.com.githubuserapp.database.FavoriteUser
+import android.example.com.githubuserapp.database.FavoriteUserDao
+import android.example.com.githubuserapp.database.FavoriteUserDatabase
+import androidx.lifecycle.LiveData
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class FavoriteUserRepository(application: Application) {
+    private val mFavoriteUserDao: FavoriteUserDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val db = FavoriteUserDatabase.getDatabase(application)
+        mFavoriteUserDao = db.favoriteUserDao()
+    }
+
+    fun getAllFavoriteUsers() : LiveData<List<FavoriteUser>> = mFavoriteUserDao.getAllFavoriteUsers()
+
+    fun insert(favoriteUser: FavoriteUser) {
+        executorService.execute { mFavoriteUserDao.insert(favoriteUser) }
+    }
+
+    fun update(favoriteUser: FavoriteUser) {
+        executorService.execute { mFavoriteUserDao.update(favoriteUser) }
+    }
+
+    fun delete(favoriteUser: FavoriteUser) {
+        executorService.execute { mFavoriteUserDao.delete(favoriteUser) }
+    }
+}
